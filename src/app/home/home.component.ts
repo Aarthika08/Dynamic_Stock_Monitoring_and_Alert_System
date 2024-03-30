4c298adb59e0a07dd7145136174249671ca2472d
Aarthika008 <aarthika@student.tce.edu>
2024-03-30 18:54:39 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { LoginService } from '../login/loginservice'
 // login.component.ts
 import { Component, OnInit } from '@angular/core';
-declare let Email: any;
 
 @Component({
   selector: 'app-home',
@@ -10,24 +9,6 @@ declare let Email: any;
 })
 export class HomeComponent  {
 
-  
-  // sendEmail() {
-  //   Email.send({
-  //     Host: "smtp.elasticemail.com",
-  //     Username: "19ifte048@ldc.edu.in",
-  //     Password: "0C589BEA17D93DD3B70BE7D58EBE02BC3CBA",
-  //     To: 'maarthika01@gmail.com',
-  //     From: "19ifte048@ldc.edu.in",
-  //     Subject: "This is the subject",
-  //     Body: "And this is the body testing1"
-  //   }).then(
-  //     (message: unknown) => {
-  //       console.log(message); // Log the message to the console
-  //       alert("Email sent successfully");
-  //     }
-  //   ).catch((error:any) => {
-  //     console.error('Error sending email:', error);
-  //     alert("Error sending email. Please try again later.");
-  //   });
-  // }
+ 
+ 
 }
