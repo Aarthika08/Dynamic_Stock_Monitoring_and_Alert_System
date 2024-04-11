43f54f12ba9167b93bb1f6ded9e074d3f905e35e
Aarthika08 <79691897+Aarthika08@users.noreply.github.com>
2024-04-11 15:04:09 +0530

    Update outgoing-stock.service.ts

--- src/app/stock-management/stocks/outgoing-stock/outgoing-stock.service.ts
+++ src/app/stock-management/stocks/outgoing-stock/outgoing-stock.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
